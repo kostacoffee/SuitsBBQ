b6255ad690c9debfeee497befe8fed9551b99d86
Kosta Dunn <kostacoffee@gmail.com>
2016-03-17 18:39:44 +1100

    Added a spinner loading icon while the members query is loading. Now processing members and adding the bbq and drink fields on download. Added a getAttendees function to return everyone who attended an event. Added an analytics button.

--- controllers.js
+++ controllers.js
@@ -1,10 +1,11 @@
 var appControllers = angular.module('appControllers', []);
 
 appControllers.controller('viewUsers', function($scope, $mdDialog, userService){
-	/*userService.getUsers().then(function(members) { 
+	$scope.isLoading = true;
+	userService.getUsers().then(function(members) { 
 		$scope.users = members;
-	});*/
-	$scope.users = [{"first": "asd", "last": "dsa", "access":1234}, {"first": "test", "last": "test", "access":1}];
+		$scope.isLoading = false;
+	});
 	$scope.showDialog = function (ev, user) {
 		$mdDialog.show({
 			controller : 'dialogController',
--- services.js
+++ services.js
@@ -1,13 +1,32 @@
 var appServices = angular.module('appServices', []);
 
 appServices.service('userService', function($http){
+	var members = [];
 	this.getUsers = function() {
 		return $http({
 			method: 'GET',
 			url: 'https://suits:electric@members.suits.org.au/query',
 			withCredentials: true
 		}).then(function(response) {
-			return response.data.members; 
+			members = response.data.members;
+			for (var i = 0; i < members.length; i++){
+				var member = members[i];
+				member.boughtBBQ = true;
+				member.drinks = 0;
+				if (member.access == null)
+					member.access = 'NO ACCESS';
+			}
+			return members; 
 		});
+	}
+	this.getAttendees = function() {
+		var attendees = []
+		for (var i = 0; i < members.length; i++){
+			var member = members[i];
+			if (member.boughtBBQ || member.drinks > 0){
+				attendees.push(member);
+			}
+		}
+		return attendees;
 	}
 });
